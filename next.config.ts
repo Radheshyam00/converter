e81334f9999b8eccfa324854e5bd4d0f041ee434
Radheshyam00 <radheshyamjanwa666@gmail.com>
2026-09-08 10:51:33 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.38.199.112"],
+  allowedDevOrigins: ["192.168.252.1"],
 };
 
 export default nextConfig;
